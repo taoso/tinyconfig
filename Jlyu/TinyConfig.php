b4708a73a7376022adce9d0e86bd8209d6328caf
Jefferson Lyu <jefferson.lyu@gmail.com>
2013-12-16 08:31:41 +0000

    only override configuration defined in file with larger number

--- Jlyu/TinyConfig.php
+++ Jlyu/TinyConfig.php
@@ -9,14 +9,18 @@ class TinyConfig {
                 continue;
             }
 
-            $config = new \stdClass();
-            foreach (require "$path/$file" as $key => $value) {
-                $config->$key = $value;
-            }
-
             $nameStart = strpos($file, '-');
             $nameEnd = strpos($file, '.');
             $group = substr($file, $nameStart + 1, $nameEnd - $nameStart - 1);
+            if (property_exists($this, $group)) {
+                $config = $this->$group;
+            } else {
+                $config = new \stdClass();
+            }
+
+            foreach (require "$path/$file" as $key => $value) {
+                $config->$key = $value;
+            }
 
             $this->$group = $config;
         }
---
+++ conf/1-hello.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'bar' => 1
+);
